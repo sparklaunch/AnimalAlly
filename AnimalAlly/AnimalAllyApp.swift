40842e5888e54abec42f6bc6081afdcb99dd7798
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-06-14 17:26:29 +0900

    Initial Commit

---
+++ AnimalAlly/AnimalAllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalAllyApp.swift
+//  AnimalAlly
+//
+//  Created by Jinwook Kim on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalAllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
